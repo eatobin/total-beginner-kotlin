d71d8d225ff2959027d9e5ea7b6f7eaecfb47248
Eric Tobin <eatobin@gmail.com>
2018-11-08 20:14:35 -0600

    replace mixed tests

--- src/test/kotlin/totalBeginner/BorrowerTest.kt
+++ src/test/kotlin/totalBeginner/BorrowerTest.kt
@@ -1,6 +1,5 @@
 package totalBeginner
 
-//import junit.framework.TestCase
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import totalBeginner.Borrower.Companion.borrowerToString
@@ -13,22 +12,22 @@ private val br1 = Borrower("Borrower1", 1)
 
 class BorrowerTest : StringSpec({
     "getName should return the Borrower name" {
-        getName(br1) shouldBe "Borrower1"
+        getName(br1).shouldBe("Borrower1")
     }
 
     "setName should set the Borrower name" {
-        setName("Borrower1", Borrower("Jack", 1)) shouldBe br1
+        setName("Borrower1", Borrower("Jack", 1)).shouldBe(br1)
     }
 
     "getMaxBooks should return the Borrower maxBooks" {
-        getMaxBooks(br1) shouldBe 1
+        getMaxBooks(br1).shouldBe(1)
     }
 
     "setMaxBooks should set the Borrower maxBooks" {
-        setMaxBooks(11, br1) shouldBe Borrower("Borrower1", 11)
+        setMaxBooks(11, br1).shouldBe(Borrower("Borrower1", 11))
     }
 
     "the Borrower string should print" {
-        borrowerToString(br1) shouldBe "Borrower1 (1 books)"
+        borrowerToString(br1).shouldBe("Borrower1 (1 books)")
     }
 })
