f767650daa1685db8f0f8ebac935860f40f4ead2
Eric Tobin <eatobin@gmail.com>
2019-01-30 11:04:47 -0700

    booktest progress

--- src/test/kotlin/total_beginner/BookTest.kt
+++ src/test/kotlin/total_beginner/BookTest.kt
@@ -2,8 +2,11 @@ package total_beginner
 
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import total_beginner.Book.Companion.getAuthor
+import total_beginner.Book.Companion.getBorrower
 import total_beginner.Book.Companion.getTitle
 import total_beginner.Book.Companion.setAuthor
+import total_beginner.Book.Companion.setBorrower
 
 private val br1 = Borrower("Borrower1", 1)
 private val bk1 = Book("Title1", "Author1", br1)
@@ -14,56 +17,34 @@ class BookTest : StringSpec({
         getTitle(bk1).shouldBe("Title1")
     }
 
-    "setAuthor should set the Book Author" {
+    "getAuthor should return the Book author" {
+        getAuthor(bk2).shouldBe("Author2")
+    }
+
+    "setAuthor should set the Book author" {
         setAuthor("Author11", bk1).shouldBe(Book(title = "Title1", author = "Author11", borrower = br1))
     }
 
-//    "getMaxBooks should return the Borrower maxBooks" {
-//        Borrower.getMaxBooks(br1).shouldBe(1)
-//    }
-//
-//    "setMaxBooks should set the Borrower maxBooks" {
-//        Borrower.setMaxBooks(11, br1).shouldBe(Borrower("Borrower1", 11))
-//    }
-//
-//    "the Borrower string should print" {
-//        Borrower.borrowerToString(br1).shouldBe("Borrower1 (1 books)")
-//    }
+    "getBorrower-someone should get the Book borrower" {
+        getBorrower(bk1).shouldBe(br1)
+    }
+
+    "getBorrower-null should get the Book borrower - null" {
+        getBorrower(bk2).shouldBe(null)
+    }
+
+    "setBorrower-someone should set the Book borrower" {
+        val nbr = Book(title = "Title2", author = "Author2",
+                borrower = Borrower(name = "BorrowerNew", maxBooks = 111))
+        setBorrower(Borrower(name = "BorrowerNew", maxBooks = 111), bk2).shouldBe(nbr)
+    }
+
 })
 
-//class BookTest : TestCase() {
-//
-//    private val br1 = Borrower("Borrower1", 1)
-//    private val bk1 = Book("Title1", "Author1", br1)
-//    private val bk2 = Book("Title2", "Author2", null)
-//
-//    fun testGetTitle() {
-//        assertEquals("Title1", getTitle(bk1))
-//    }
-//
-//    fun testGetAuthor() {
-//        assertEquals("Author2", getAuthor(bk2))
-//    }
-//
-//    fun testSetAuthor() {
-//        assertEquals(Book(title = "Title1", author = "Author11", borrower = br1), setAuthor("Author11", bk1))
-//    }
-//
-//    fun testGetBorrowerSomeone() {
-//        assertEquals(br1, getBorrower(bk1))
-//    }
-//
-//    fun testGetBorrowerNull() {
-//        assertNull(getBorrower(bk2))
-//    }
-//
-//    fun testSetBorrowerSomeone() {
-//        val nbr = Book(title = "Title2", author = "Author2",
-//                borrower = Borrower(name = "BorrowerNew", maxBooks = 111))
-//        assertEquals(nbr,
-//                setBorrower(Borrower(name = "BorrowerNew", maxBooks = 111), bk2))
-//    }
-//
+
+
+
+
 //    fun testSetBorrowerNull() {
 //        val nbr = Book(title = "Title1", author = "Author1",
 //                borrower = null)
