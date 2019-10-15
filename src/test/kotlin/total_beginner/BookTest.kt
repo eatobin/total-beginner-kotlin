d4418d3afa5897ceaa50a6775e1fd38c82e741e3
Eric Tobin <eatobin@gmail.com>
2019-10-15 09:59:53 -0500

    book tests

--- src/test/kotlin/total_beginner/BookTest.kt
+++ src/test/kotlin/total_beginner/BookTest.kt
@@ -2,13 +2,11 @@ package total_beginner
 
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
-import total_beginner.Book.Companion.bookToString
 import total_beginner.Book.Companion.getAuthor
 import total_beginner.Book.Companion.getBorrower
 import total_beginner.Book.Companion.getTitle
 import total_beginner.Book.Companion.setAuthor
 import total_beginner.Book.Companion.setBorrower
-import kotlin.test.assertEquals
 
 private val br1 = Borrower("Borrower1", 1)
 private val bk1 = Book("Title1", "Author1", br1)
@@ -40,20 +38,6 @@ class BookTest : StringSpec({
         val nbr = Book(title = "Title2", author = "Author2",
                 borrower = Borrower(name = "BorrowerNew", maxBooks = 111))
         setBorrower(Borrower(name = "BorrowerNew", maxBooks = 111), bk2).shouldBe(nbr)
-    }
-
-    fun testSetBorrowerNull() {
-        val nbr = Book(title = "Title1", author = "Author1",
-                borrower = null)
-        assertEquals(nbr, setBorrower(null, bk1))
-    }
-
-    fun testBookToStringSomeone() {
-        assertEquals("Title1 by Author1; Checked out to Borrower1", bookToString(bk1))
-    }
-
-    fun testBookToStringNull() {
-        assertEquals("Title2 by Author2; Available", bookToString(bk2))
     }
 
 })
