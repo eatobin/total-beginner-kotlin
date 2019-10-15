317ec24ae3f95af2071cb3ca321fc9407bc1e2a8
Eric Tobin <eatobin@gmail.com>
2019-10-15 17:24:09 -0500

    readFileIntoJsonString need fixing

--- src/main/kotlin/total_beginner/App.kt
+++ src/main/kotlin/total_beginner/App.kt
@@ -1,81 +1,81 @@
 package total_beginner
 
-//
-//import total_beginner.Library.addItem
+
+import total_beginner.Library.addItem
 //import total_beginner.Library.borrowersToJsonString
-//import total_beginner.Library.checkIn
-//import total_beginner.Library.checkOut
+import total_beginner.Library.checkIn
+import total_beginner.Library.checkOut
 //import total_beginner.Library.jsonStringToBooks
 //import total_beginner.Library.jsonStringToBorrowers
-//import total_beginner.Library.statusToString
-//import java.io.BufferedReader
-//import java.io.File
-//
-//object App {
-//
-//    @JvmStatic
-//    fun main(args: Array<String>) {
-//
-//        var tvBorrowers: Borrowers = emptyList()
-//        var tvBooks: Books = emptyList()
-//
-//        val jsonBorrowersFileBefore = "borrowers-before.json"
-//        val jsonBooksFile = "books-before.json"
-//        val jsonBorrowersFileAfter = "borrowers-after.json"
-//        val jsonBorrowersFileBad = "bad-borrowers.json"
-//        val emptyFile = "empty.json"
-//
-//        tvBorrowers = addItem(Borrower("Jim", 3), tvBorrowers)
-//        tvBorrowers = addItem(Borrower("Sue", 3), tvBorrowers)
-//        tvBooks = addItem(Book("War And Peace", "Tolstoy", null), tvBooks)
-//        tvBooks = addItem(Book("Great Expectations", "Dickens", null), tvBooks)
-//        println("\nJust created new library")
-//        println(statusToString(tvBooks, tvBorrowers))
-//
-//        println("Check out War And Peace to Sue")
-//        tvBooks = checkOut("Sue", "War And Peace", tvBorrowers, tvBooks)
-//        println(statusToString(tvBooks, tvBorrowers))
-//
-//        println("Now check in War And Peace from Sue...")
-//        tvBooks = checkIn("War And Peace", tvBooks)
-//        println("...and check out Great Expectations to Jim")
-//        tvBooks = checkOut("Jim", "Great Expectations", tvBorrowers, tvBooks)
-//        println(statusToString(tvBooks, tvBorrowers))
-//
-//        println("Add Eric and The Cat In The Hat")
-//        tvBorrowers = addItem(Borrower("Eric", 1), tvBorrowers)
-//        tvBooks = addItem(Book("The Cat In The Hat", "Dr. Seuss", null), tvBooks)
-//        println("Check Out Dr. Seuss to Eric")
-//        tvBooks = checkOut("Eric", "The Cat In The Hat", tvBorrowers, tvBooks)
-//        println(statusToString(tvBooks, tvBorrowers))
-//
-//        println("Now let's do some BAD stuff...\n")
-//
-//        println("Add a borrower that already exists (Borrower('Jim', 3))")
-//        tvBorrowers = addItem(Borrower("Jim", 3), tvBorrowers)
-//        println("No change to Test Library:")
-//        println(statusToString(tvBooks, tvBorrowers))
-//
-//        println("Add a book that already exists (Book('War And Peace', 'Tolstoy'))")
-//        tvBooks = addItem(Book("War And Peace", "Tolstoy", null), tvBooks)
-//        println("No change to Test Library:")
-//        println(statusToString(tvBooks, tvBorrowers))
-//
-//        println("Check out a valid book to an invalid person (checkOut('JoJo', 'War And Peace', borrowers))")
-//        tvBooks = checkOut("JoJo", "War And Peace", tvBorrowers, tvBooks)
-//        println("No change to Test Library:")
-//        println(statusToString(tvBooks, tvBorrowers))
-//
-//        println("Check out an invalid book to an valid person (checkOut('Sue', 'Not A Book', borrowers))")
-//        tvBooks = checkOut("Sue", "Not A Book", tvBorrowers, tvBooks)
-//        println("No change to Test Library:")
-//        println(statusToString(tvBooks, tvBorrowers))
-//
-//        println("Last - check in a book not checked out (checkIn('War And Peace'))")
-//        tvBooks = checkIn("War And Peace", tvBooks)
-//        println("No change to Test Library:")
-//        println(statusToString(tvBooks, tvBorrowers))
-//
+import total_beginner.Library.statusToString
+import java.io.BufferedReader
+import java.io.File
+
+object App {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        var tvBorrowers: Borrowers = emptyList()
+        var tvBooks: Books = emptyList()
+
+        val jsonBorrowersFileBefore = "borrowers-before.json"
+        val jsonBooksFile = "books-before.json"
+        val jsonBorrowersFileAfter = "borrowers-after.json"
+        val jsonBorrowersFileBad = "bad-borrowers.json"
+        val emptyFile = "empty.json"
+
+        tvBorrowers = addItem(Borrower("Jim", 3), tvBorrowers)
+        tvBorrowers = addItem(Borrower("Sue", 3), tvBorrowers)
+        tvBooks = addItem(Book("War And Peace", "Tolstoy", null), tvBooks)
+        tvBooks = addItem(Book("Great Expectations", "Dickens", null), tvBooks)
+        println("\nJust created new library")
+        println(statusToString(tvBooks, tvBorrowers))
+
+        println("Check out War And Peace to Sue")
+        tvBooks = checkOut("Sue", "War And Peace", tvBorrowers, tvBooks)
+        println(statusToString(tvBooks, tvBorrowers))
+
+        println("Now check in War And Peace from Sue...")
+        tvBooks = checkIn("War And Peace", tvBooks)
+        println("...and check out Great Expectations to Jim")
+        tvBooks = checkOut("Jim", "Great Expectations", tvBorrowers, tvBooks)
+        println(statusToString(tvBooks, tvBorrowers))
+
+        println("Add Eric and The Cat In The Hat")
+        tvBorrowers = addItem(Borrower("Eric", 1), tvBorrowers)
+        tvBooks = addItem(Book("The Cat In The Hat", "Dr. Seuss", null), tvBooks)
+        println("Check Out Dr. Seuss to Eric")
+        tvBooks = checkOut("Eric", "The Cat In The Hat", tvBorrowers, tvBooks)
+        println(statusToString(tvBooks, tvBorrowers))
+
+        println("Now let's do some BAD stuff...\n")
+
+        println("Add a borrower that already exists (Borrower('Jim', 3))")
+        tvBorrowers = addItem(Borrower("Jim", 3), tvBorrowers)
+        println("No change to Test Library:")
+        println(statusToString(tvBooks, tvBorrowers))
+
+        println("Add a book that already exists (Book('War And Peace', 'Tolstoy'))")
+        tvBooks = addItem(Book("War And Peace", "Tolstoy", null), tvBooks)
+        println("No change to Test Library:")
+        println(statusToString(tvBooks, tvBorrowers))
+
+        println("Check out a valid book to an invalid person (checkOut('JoJo', 'War And Peace', borrowers))")
+        tvBooks = checkOut("JoJo", "War And Peace", tvBorrowers, tvBooks)
+        println("No change to Test Library:")
+        println(statusToString(tvBooks, tvBorrowers))
+
+        println("Check out an invalid book to an valid person (checkOut('Sue', 'Not A Book', borrowers))")
+        tvBooks = checkOut("Sue", "Not A Book", tvBorrowers, tvBooks)
+        println("No change to Test Library:")
+        println(statusToString(tvBooks, tvBorrowers))
+
+        println("Last - check in a book not checked out (checkIn('War And Peace'))")
+        tvBooks = checkIn("War And Peace", tvBooks)
+        println("No change to Test Library:")
+        println(statusToString(tvBooks, tvBorrowers))
+
 //        println("Okay... let's finish with some persistence. First clear the whole library:")
 //        tvBorrowers = emptyList()
 //        tvBooks = emptyList()
@@ -123,19 +123,19 @@ package total_beginner
 ////
 ////        println("And... that's all...")
 ////        println("Thanks - bye!\n")
-//
-//    }
-//
-//    private fun readFileIntoJsonString(fp: String): jsonString? {
-//        return try {
-//            val bufferedReader: BufferedReader = File(fp).bufferedReader()
-//            bufferedReader.use { it.readText() }
-//        } catch (_: Exception) {
-//            println("The requested file $fp does not exist..")
-//            null
-//        }
-//    }
-//
+
+    }
+
+    // TODO
+    private fun readFileIntoJsonString(fp: String): jsonString {
+        return try {
+            val bufferedReader: BufferedReader = File(fp).bufferedReader()
+            bufferedReader.use { it.readText() }
+        } catch (e: Exception) {
+            println("The requested file $fp does not exist..")
+        }
+    }
+
 //    private fun newBooksFromJsonString(bksfp: String): Books {
 //        val mJsonBksStr: jsonString? = readFileIntoJsonString(bksfp)
 //        return if (mJsonBksStr != null) {
@@ -154,4 +154,4 @@ package total_beginner
 //        File("borrowers-after.json").bufferedWriter().use { out -> out.write(js) }
 //    }
 //
-//}
+}
