de503b2685df5623bd784e4c6646f58e8bef803a
Eric Tobin <eatobin@gmail.com>
2019-10-15 21:49:24 -0500

    broken

--- src/test/kotlin/total_beginner/LibraryTest.kt
+++ src/test/kotlin/total_beginner/LibraryTest.kt
@@ -40,6 +40,7 @@ private const val jsonStringBorrowers = """[{"name":"Borrower1","maxBooks":1},{"
 private const val jsonStringBorrowersBad = """[{"nameX":"Borrower1","maxBooks":1},{"name":"Borrower2","maxBooks":2}]"""
 private const val jsonStringBooks = """[{"title":"Title1","author":"Author1","borrower":{"name":"Borrower1","maxBooks":1}},{"title":"Title2","author":"Author2","borrower":null}]"""
 private const val jsonStringBooksBad = """[{"title":"Title1","authorX":"Author1","borrower":{"name":"Borrower1","maxBooks":1}},{"title":"Title2","author":"Author2","borrower":null}]"""
+private const val jsonStringBooksBadFile = """[{"title";"Title1","author":"Author1","borrower":{"name":"Borrower1","maxBooks":1}},{"title":"Title2","author":"Author2","borrower":null}]"""
 private const val jsonStringBooksNoBorrowerOn2 = """[{"title":"Title1","author":"Author1","borrower":{"name":"Borrower1","maxBooks":1}},{"title":"Title2","author":"Author2"}]"""
 
 private const val ss = "\n--- Status Report of Test Library ---\n\nTest Library: 3 books; 3 borrowers.\n\nTitle1 by Author1; Checked out to Borrower1\nTitle2 by Author2; Available\nTitle3 by Author3; Checked out to Borrower3\n\nBorrower1 (1 books)\nBorrower2 (2 books)\nBorrower3 (3 books)\n\n--- End of Status Report ---\n"
@@ -107,6 +108,10 @@ class LibraryTest : StringSpec({
             jsonStringToBooks(jsonStringBooksBad)
         }
         bookException.message shouldStartWith ("Unable to instantiate Book with parameters")
+        val bookExceptionFile = shouldThrow<com.beust.klaxon.KlaxonException> {
+            jsonStringToBooks(jsonStringBooksBadFile)
+        }
+        bookExceptionFile.message shouldStartWith ("Unexpected character at position 9: ';'")
     }
 
     "objects should convert to a JSON string" {
